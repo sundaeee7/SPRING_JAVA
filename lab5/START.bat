d621a5a7d230b2a9a6ce5e62ac5aac96305dd190
sundaeee7 <andreykapystin200@mail.ru>
2025-11-16 18:58:30 +0700

    Delete lab5

--- lab5/START.bat
+++
@@ -1,9 +0,0 @@
-psql -U postgres
-ALTER USER postgres WITH PASSWORD 'abcd';
-CREATE DATABASE bookdb;
-\q
-psql -U postgres -d bookdb -f init.sql
-
-mvn "exec:java" "-Dexec.mainClass=com.example.lab5.Lab5Application"
-
-pause
